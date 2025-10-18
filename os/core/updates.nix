745856a0f49c4f976520a8d5c04a887077f001c5
noskisdead <noskisdead@users.noreply.github.com>
2025-10-18 18:21:01 -0400

    added custom updates

--- os/core/updates.nix
+++ os/core/updates.nix
@@ -1,5 +1,26 @@
 { config, ... }:
 
 {
-  # setup autoupgrade
+  # Updating
+  system.autoUpgrade = {
+    enable = true;
+    dates = "daily";
+    flake = "github:noskisdead/flake";
+    flags = [
+      "--update-input"
+      "nixpkgs"
+    ];
+  };
+  
+  # Garbage Collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 5d";
+  };
+
+  # Nix Store Optimise
+  nix.optimise = {
+    automatic = true;
+  };
 }
