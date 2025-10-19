9ca8001e34be2c4ff50a0418b08dc3d97bec0bb7
noskisdead <noskisdead@users.noreply.github.com>
2025-10-19 00:30:49 -0400

    installed home-manager

--- flake.nix
+++ flake.nix
@@ -3,13 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, ... }@inputs: {
+  outputs = { nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.pen = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./hosts/pen/configuration.nix
+	home-manager.nixosModules.home-manager
+	{
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+	  home-manager.users.kenny = import ./hosts/pen/home.nix;
+	}
       ];
     };
   };
---
+++ hosts/pen/home.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "kenny";
+  home.homeDirectory = "/home/kenny";
+  home.stateVersion = "25.05"; # do not change
+  
+  imports = [
+    ./../../hm/default.nix # Import all your Home Manager modules
+  ];
+  
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Allow unfree software
+  nixpkgs.config.allowUnfree = true;
+}
