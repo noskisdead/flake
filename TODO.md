b92270c381cf8543014d473725edf50a52a6f00b
noskisdead <noskisdead@users.noreply.github.com>
2025-10-19 01:51:21 -0400

    finished for today

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# To do list
+- [ ] Setup MPV as default with ModernX and ThumbFast
+- [ ] Setup Zen Browser with flatpak
+- [ ] Setup LazyVim with the flake
+- [ ] Rice KDE with plasma-manager and Latte Dock
---
+++ hm/programs/zen-browser.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  programs.zen-browser = {
+    policies = {
+    AutofillAddressEnabled = true;
+    AutofillCreditCardEnabled = false;
+    DisableAppUpdate = true;
+    DisableFeedbackCommands = true;
+    DisableFirefoxStudies = true;
+    DisablePocket = true;
+    DisableTelemetry = true;
+    DontCheckDefaultBrowser = true;
+    NoDefaultBookmarks = true;
+    OfferToSaveLogins = false;
+    EnableTrackingProtection = {
+      Value = true;
+      Locked = true;
+      Cryptomining = true;
+      Fingerprinting = true;
+    };
+  };
+}
