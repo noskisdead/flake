df225582fc0f9a8125c14647cb1ed3f09146b2b3
noskisdead <noskisdead@users.noreply.github.com>
2025-10-19 01:14:53 -0400

    obsidian

---
+++ hm/default.nix
@@ -0,0 +1,7 @@
+{ config, ... }:
+
+{
+  imports = [
+    ./programs/productivity.nix
+  ];
+}
--- os/core/settings.nix
+++ os/core/settings.nix
@@ -1,6 +1,7 @@
 { config, ... }:
 
 {
+  security.apparmor.enable = true;
   nixpkgs.config.allowUnfree = true; # Allow unfree packages
   nix.settings.experimental-features = [ "nix-command" "flakes" ]; # Enables support for flakes
   system.stateVersion = "25.05"; # dont change it por favor :)
